3f17b4b5b2f1a3bd4cf56d9319ffb05c472073ca
LEE-SungBin <rqtoe@snu.ac.kr>
2023-07-24 13:42:27 +0900

    AOUP initializae

---
+++ AOUP.py
@@ -0,0 +1,256 @@
+import numpy as np
+import numpy.typing as npt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from dataclasses import dataclass, field, asdict
+from datetime import datetime
+import pandas as pd
+from pathlib import Path
+from typing import Any, Self
+import hashlib
+import json
+import pickle
+import argparse
+import time
+
+
+@dataclass
+class Parameter:
+    N_particle: int
+    N_ensemble: int
+    velocity: float
+    Lambda: float
+    boundary: float
+    N_bins: int
+    gamma: float
+    slope: float
+    temperature: float
+    tau: float
+    Da: float
+    delta_t: float
+    initial: int
+    sampling: int
+
+
+class AOUP:
+    def __init__(self, parameter: Parameter):
+        self.parameter = parameter
+
+        self.set_coeff()
+        self.set_zero()
+
+    def set_coeff(self) -> None:
+        self.N_particle = self.parameter.N_particle  # * number of AOUPs
+        self.N_ensemble = self.parameter.N_ensemble  # * number of ensemble
+        self.velocity = self.parameter.velocity  # * velocity of object
+        self.Lambda = self.parameter.Lambda      # * size of object
+        self.boundary = self.parameter.boundary  # * size of periodic boundary
+        self.N_bins = self.parameter.N_bins      # * number of histogram bins
+        self.gamma = self.parameter.gamma        # * drag coefficient
+        self.slope = self.parameter.slope        # * slope of potential
+        self.temperature = self.parameter.temperature    # * temperature
+        # * autocorrelation time-scale of colored noise
+        self.tau = self.parameter.tau
+        self.Da = self.parameter.Da              # * coefficients of colored noise
+        self.delta_t = self.parameter.delta_t    # * update time interval
+        # * number of iter to remove initial effect
+        self.initial = self.parameter.initial
+        self.sampling = self.parameter.sampling  # * number of iter to collect samples
+
+    def set_zero(self) -> None:  # * initialize position and noise
+        rng = np.random.default_rng()
+
+        self.position = rng.uniform(
+            low=-self.boundary/2, high=self.boundary/2, size=(self.N_ensemble, self.N_particle))
+
+        self.colored_noise = rng.normal(
+            loc=0.0, scale=1.0, size=(self.N_ensemble, self.N_particle))
+
+    def get_result(self) -> None:  # * get average and std drag of given parameter
+        now = time.perf_counter()
+        for _ in range(self.initial):
+            self.time_evolution()  # * update self.position and self.colored_noise
+
+        drag = []
+        for _ in range(self.sampling):
+            self.time_evolution()
+            drag.append(self.get_drag())
+
+        key = hashlib.sha1(str(parameter).encode()).hexdigest()[:6]
+
+        data_dir, setting_dir = Path("data"), Path("setting")
+
+        data_dir.mkdir(parents=True, exist_ok=True)
+        setting_dir.mkdir(parents=True, exist_ok=True)
+
+        name = key
+
+        output = {
+            "key": key,
+        }
+
+        output.update(asdict(self.parameter))
+
+        with open(setting_dir / f"{name}.json", "w") as file:
+            json.dump(output, file)
+
+        result = {
+            "average": np.mean(drag),
+            "std": np.std(drag),
+            "time": time.perf_counter()-now
+        }
+
+        output.update(result)
+
+        with open(data_dir / f"{name}.pkl", "wb") as file:
+            pickle.dump(output, file)
+
+    def animation(self) -> None:  # * animate histogram
+        self.fig, self.ax = plt.subplots(tight_layout=True)
+        self.bins = np.linspace(-self.boundary/2,
+                                self.boundary/2, self.N_bins+1)
+
+        self.ax.hist(self.position.reshape(-1), bins=self.bins)
+
+        ani = animation.FuncAnimation(
+            fig=self.fig, func=self.update, frames=300, interval=0, blit=False)
+
+        ani.save(f"test.mp4", fps=30, extra_args=['-vcodec', 'libx264'])
+
+    def update(self, i: int) -> None:  # * update animation
+        self.time_evolution()
+
+        self.ax.cla()
+        self.ax.hist(self.position.reshape(-1), bins=self.bins)
+        self.ax.axvline(-self.Lambda/2, linestyle="--", color="k")
+        self.ax.axvline(0.0, linestyle="--", color="k")
+        self.ax.axvline(self.Lambda/2, linestyle="--", color="k")
+        self.ax.axhline(self.N_particle*self.N_ensemble /
+                        self.N_bins, linestyle="--", color="k")
+
+        lx = np.linspace(-self.Lambda/2, 0, 10)
+        rx = np.linspace(0, self.Lambda/2, 10)
+
+        def f(x: npt.NDArray):
+            return self.N_particle*self.N_ensemble/self.N_bins * (1 + x / (self.Lambda / 2))
+
+        def g(x: npt.NDArray):
+            return self.N_particle*self.N_ensemble/self.N_bins * (1 - x / (self.Lambda / 2))
+
+        self.ax.plot(lx, f(lx), color="b")
+        self.ax.plot(rx, g(rx), color="r")
+
+        self.ax.set_xlim(-self.boundary/2, self.boundary/2)
+        self.ax.set_ylim(0, self.N_particle*self.N_ensemble/self.N_bins * 1.5)
+
+        self.ax.text(
+            0.99, 0.99, f"iter = {i+1}",
+            verticalalignment="top", horizontalalignment='right',
+            transform=self.ax.transAxes,
+            color='black', fontsize=20
+        )
+
+        self.ax.text(
+            0.99, 0.91, f"drag = {self.get_drag()}",
+            verticalalignment="top", horizontalalignment='right',
+            transform=self.ax.transAxes,
+            color='black', fontsize=20
+        )
+
+    def get_drag(self) -> float:  # * calculate drag force
+        positive_drag = self.slope * sum(
+            1 for i in self.position.reshape(-1) if 0 <
+            i < self.Lambda / 2) / self.N_ensemble
+
+        negative_drag = self.slope * sum(
+            1 for i in self.position.reshape(-1) if -
+            self.Lambda / 2 < i < 0) / self.N_ensemble
+
+        return positive_drag - negative_drag
+
+    def time_evolution(self) -> None:  # * time evolution of AOUPs
+        rng = np.random.default_rng()
+
+        self.colored_noise += - self.colored_noise / self.tau * self.delta_t
+        self.colored_noise += rng.normal(
+            loc=0.0,  # * mean
+            scale=np.sqrt(2 * self.Da / self.tau * self.delta_t),  # * std
+            size=(self.N_ensemble, self.N_particle)
+        )
+
+        force = self.get_force()
+
+        self.position += (force / self.gamma - self.velocity) * self.delta_t
+        self.position += self.colored_noise * self.delta_t
+        self.position += rng.normal(
+            loc=0.0,
+            scale=np.sqrt(2 * self.temperature / self.gamma * self.delta_t),
+            size=(self.N_ensemble, self.N_particle)
+        )
+
+        self.position = self.periodic_boundary(self.position)
+
+    def get_force(self) -> npt.NDArray:  # * get external force from object
+        force = np.zeros(shape=(self.N_ensemble, self.N_particle))
+
+        force = np.where(np.array(
+            [- self.Lambda / 2 < self.position, self.position < 0.0]).all(0), -self.slope, force)
+
+        force = np.where(np.array(
+            [0.0 < self.position, self.position < self.Lambda / 2]).all(0), self.slope, force)
+
+        return force
+
+    # * periodic boundary condition
+    def periodic_boundary(self, position: npt.NDArray) -> npt.NDArray:
+        position = np.where(position > self.boundary / 2,
+                            position - self.boundary * (((position - self.boundary/2) / self.boundary).astype(int) + 1), position)
+
+        position = np.where(position < - self.boundary / 2,
+                            position + self.boundary * ((-(position + self.boundary/2) / self.boundary).astype(int) + 1), position)
+
+        return position
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-N", "--N_particle", type=int, default=10000)
+    parser.add_argument("-ens", "--N_ensemble", type=int, default=10)
+    parser.add_argument("-v", "--velocity", type=float, default=1.0)
+    parser.add_argument("-d", "--Lambda", type=float, default=1.0)
+    parser.add_argument("-L", "--boundary", type=float, default=5.0)
+    parser.add_argument("-bin", "--N_bins", type=int, default=40)
+    parser.add_argument("-g", "--gamma", type=float, default=1.0)
+    parser.add_argument("-f", "--slope", type=float, default=2.0)
+    parser.add_argument("-T", "--temperature", type=float, default=1.0)
+    parser.add_argument("-tau", "--tau", type=float, default=1.0)
+    parser.add_argument("-Da", "--Da", type=float, default=1.0)
+    parser.add_argument("-dt", "--delta_t", type=float, default=0.01)
+    parser.add_argument("-init", "--initial", type=int, default=1000)
+    parser.add_argument("-sam", "--sampling", type=int, default=1000)
+
+    args = parser.parse_args()
+
+    parameter = Parameter(
+        N_particle=args.N_particle,
+        N_ensemble=args.N_ensemble,
+        velocity=args.velocity,
+        Lambda=args.Lambda,
+        boundary=args.boundary,
+        N_bins=args.N_bins,
+        gamma=args.gamma,
+        slope=args.slope,
+        temperature=args.temperature,
+        tau=args.tau,
+        Da=args.Da,
+        delta_t=args.delta_t,
+        initial=args.initial,
+        sampling=args.sampling,
+    )
+
+    print(parameter)
+
+    aoup = AOUP(parameter)
+    # aoup.animation()
+    aoup.get_result()
---
+++ manage_data.py
@@ -0,0 +1,173 @@
+from __future__ import annotations
+
+import pickle
+from dataclasses import asdict, dataclass, field
+from datetime import datetime
+from pathlib import Path
+from typing import Any, Self
+
+import numpy as np
+import numpy.typing as npt
+import json
+import pickle
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.animation as animation
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.axes import Axes
+
+from AOUP import Parameter
+
+
+def get_conditions(
+    N_particle: int | None = None,
+    N_ensemble: int | None = None,
+    velocity: float | None = None,
+    Lambda: float | None = None,
+    boundary: float | None = None,
+    N_bins: int | None = None,
+    gamma: float | None = None,
+    slope: float | None = None,
+    temperature: float | None = None,
+    tau: float | None = None,
+    Da: float | None = None,
+    delta_t: float | None = None,
+    initial: int | None = None,
+    sampling: int | None = None,
+) -> list[str]:
+
+    conditions: list[str] = []
+
+    if N_particle is not None:
+        conditions.append(f"N_particle == {N_particle}")
+    if N_ensemble is not None:
+        conditions.append(f"N_ensemble == {N_ensemble}")
+    if velocity is not None:
+        conditions.append(f"velocity == {velocity}")
+    if Lambda is not None:
+        conditions.append(f"Lambda == {Lambda}")
+    if boundary is not None:
+        conditions.append(f"boundary == {boundary}")
+    if N_bins is not None:
+        conditions.append(f"N_bins = {N_bins}")
+    if gamma is not None:
+        conditions.append(f"gamma == {gamma}")
+    if slope is not None:
+        conditions.append(f"slope == {slope}")
+    if temperature is not None:
+        conditions.append(f"temperature == {temperature}")
+    if tau is not None:
+        conditions.append(f"tau == {tau}")
+    if Da is not None:
+        conditions.append(f"Da == {Da}")
+    if delta_t is not None:
+        conditions.append(f"delta_t == {delta_t}")
+    if initial is not None:
+        conditions.append(f"initial == {initial}")
+    if sampling is not None:
+        conditions.append(f"sampling == {sampling}")
+
+    return conditions
+
+
+def get_setting(
+    location: Path = Path("."),
+    conditions: list[str] | None = None,
+) -> Any:
+
+    def filter_file(f: Path) -> bool:
+        return f.is_file() and (f.suffix == ".json") and f.stat().st_size > 0
+
+    # * Scan the setting directory and gather result files
+    setting_dir = location / f"setting"
+    setting_files = [f for f in setting_dir.iterdir() if filter_file(f)]
+
+    # * Read files
+    settings: list[dict[str, Any]] = []
+    for file in setting_files:
+        with open(file, "rb") as f:
+            settings.append(json.load(f))
+
+    df = pd.DataFrame(settings)
+
+    if conditions == None or len(conditions) == 0:
+        return df["key"]
+
+    else:
+        return df.query(" and ".join(conditions))["key"]
+
+
+def load_result(
+    location: Path = Path("."),
+    conditions: list[str] | None = None,
+) -> pd.DataFrame:
+
+    # * Scan the result directory and gather result files
+    result_dir = location / f"data"
+    result_keys = get_setting(location, conditions)
+    result_files = [result_dir /
+                    f"{result_key}.pkl" for result_key in result_keys]
+
+    # * Read files
+    results: list[dict[str, Any]] = []
+    for file in result_files:
+        with open(file, "rb") as f:
+            results.append(pickle.load(f))
+
+    # * Concatenate to single dataframe
+    df = pd.DataFrame(results)
+    return df
+
+
+def delete_result(
+    key_names: list[str],
+    location: Path = Path("."),
+) -> None:
+
+    del_setting, del_data = 0, 0
+
+    for key in key_names:
+        target_setting = location / f"setting/{key}.json"
+        target_file = location / f"data/{key}.pkl"
+
+        try:
+            target_setting.unlink()
+            del_setting += 1
+        except OSError:
+            print(f"No setting found for key in setting: {key}")
+
+        try:
+            target_file.unlink()
+            del_data += 1
+        except OSError:
+            print(f"No file found for key in data: {key}")
+
+    print(f"setting deleted: {del_setting}, data deleted: {del_data}")
+
+
+def delete_all(
+    location: Path = Path("."),
+) -> None:
+    setting_dir = location / "setting"
+    data_dir = location / "data"
+
+    settings = [f for f in setting_dir.iterdir()]
+    datas = [f for f in data_dir.iterdir()]
+
+    del_setting, del_data = 0, 0
+    for setting in settings:
+        try:
+            setting.unlink()
+            del_setting += 1
+        except OSError:
+            print(f"No setting found for key in setting: {setting}")
+    for data in datas:
+        try:
+            data.unlink()
+            del_data += 1
+        except OSError:
+            print(f"No file found for key in data: {data}")
+
+    print(f"setting deleted: {del_setting}, data deleted: {del_data}")
