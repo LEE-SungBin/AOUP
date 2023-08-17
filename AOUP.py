95937e4c0ebd824ea6cc0082411d3e14489ab510
LEE-SungBin <rqtoe@snu.ac.kr>
2023-08-17 17:06:27 +0900

    animation added

--- AOUP.py
+++ AOUP.py
@@ -6,7 +6,9 @@ from dataclasses import dataclass, field, asdict
 from datetime import datetime
 import pandas as pd
 from pathlib import Path
-from typing import Any, Self
+# * Self only available from python 3.11, please update python
+from typing import Self
+from typing import Any
 import hashlib
 import pickle
 import argparse
@@ -54,18 +56,11 @@ class Time:
             f"{key}={int(np.round(log, 0))}s" for key, log in zip(asdict(self).keys(), asdict(self).values())
         )
 
-    # def to_log(self) -> str:
-    #     return ", ".join(
-    #         f"{key}={log}s" for key, log in zip(asdict(self).keys(), asdict(self).values())
-    #     )
-
 
 class AOUP:
     def __init__(self, parameter: Parameter):
         self.parameter = parameter
 
-        self.set_coeff()
-        self.set_zero()
         self.Time = Time(
             force_update=0.0,
             positive_update=0.0,
@@ -75,6 +70,9 @@ class AOUP:
             drag_update=0.0,
             total=0.0,
         )
+
+        self.set_coeff()
+        self.set_zero()
 
     def set_coeff(self) -> None:
         self.N_particle = self.parameter.N_particle  # * number of AOUPs
@@ -98,225 +96,226 @@ class AOUP:
     def set_zero(self) -> None:  # * initialize position and noise
         self.rng = np.random.default_rng()
 
-        init_position = self.rng.uniform(
+        self.position = self.rng.uniform(
             low=-self.boundary/2, high=self.boundary/2, size=(self.N_ensemble, self.N_particle))
-        self.position = np.array([init_position, init_position])
 
         self.update_positive_negative()
 
-        init_noise = self.rng.normal(
+        self.colored_noise = self.rng.normal(
             loc=0.0, scale=1.0, size=(self.N_ensemble, self.N_particle))
-        self.colored_noise = np.array([init_noise, init_noise])
 
-    def get_result(self) -> None:  # * get average and std of drag
-        now = time.perf_counter()
+    def run_AOUP(self) -> None:  # * get average and std of drag
+        self.start = time.perf_counter()
 
         for _ in range(self.initial):
             self.time_evolution()  # * update self.position and self.colored_noise
 
         drag = np.zeros(self.N_ensemble)  # * [self.N_ensemble]
-        for i in range(self.sampling * self.interval):
-            self.time_evolution()
+
+        # * iteration = sampling * interval
+        for _ in range(self.sampling):
+            for _ in range(self.interval):
+                self.time_evolution()
             # * time average of drag (Ergodic hypothesis: time average = ensemble average)
-            if i % self.interval == self.interval - 1:
-                drag += self.get_drag()
-                # drag += self.get_drag() / self.sampling
+            drag += self.get_drag()
 
-        self.Time.total = time.perf_counter() - now
+        self.Time.total = time.perf_counter() - self.start
 
+        self.save_result(drag)
+
+    def save_result(self, drag: npt.NDArray) -> None:
         key = hashlib.sha1(str(self.parameter).encode()).hexdigest()[:6]
         data_dir, setting_dir = Path("data"), Path("setting")
         data_dir.mkdir(parents=True, exist_ok=True)
         setting_dir.mkdir(parents=True, exist_ok=True)
-        name = key
-        output = {
+        filename = key
+        output: dict[str, Any] = {
             "key": key,
         }
         output.update(asdict(self.parameter))
 
-        # with open(setting_dir / f"{name}.json", "w") as file:
-        #     json.dump(output, file)  # * save setting
+        # * save setting
+        # with open(setting_dir / f"{filename}.json", "w") as file:
+        #     json.dump(output, file)
 
-        result = {
+        # output.update(asdict(self.Time))
+        result: dict[str, Any] = {
             "drag": np.mean(drag),
             "std": np.std(drag) / np.sqrt(self.N_ensemble),
-            "time": time.perf_counter()-now,
+            "time": time.perf_counter() - self.start,
         }
         output.update(result)
 
-        with open(data_dir / f"{name}.pkl", "wb") as file:
-            pickle.dump(output, file)  # * save result
+        # * save result
+        with open(data_dir / f"{filename}.pkl", "wb") as file:
+            pickle.dump(output, file)
 
-        log = (
-            f"{datetime.now().replace(microsecond=0)} | {self.parameter.to_log()} | drag={np.round(np.mean(drag),5)}, std={np.round(np.std(drag)/np.sqrt(self.N_ensemble),5)} | {self.Time.to_log()}\n"
-        )
-
+        # * save log
         with open("log.txt", "a") as file:
-            file.write(log)  # * save log
+            file.write(
+                f"{datetime.now().replace(microsecond=0)} | {self.parameter.to_log()} | drag={np.round(np.mean(drag),5)}, std={np.round(np.std(drag)/np.sqrt(self.N_ensemble),5)} | {self.Time.to_log()}\n"
+            )
 
-    def time_evolution(self) -> None:  # * time evolution of AOUPs
-        now = time.perf_counter()
-        force = self.get_force()[1]
-        self.Time.force_update += time.perf_counter() - now
+    # * time evolution of AOUP
+    def time_evolution(self) -> None:
+        force = self.get_force()
 
         now = time.perf_counter()
-        temp_position = self.position[1] + \
-            (force / self.gamma - self.velocity) * self.delta_t
-        temp_position += self.colored_noise[1] * self.delta_t
-        temp_position += self.rng.normal(
+        self.position += (force / self.gamma - self.velocity) * self.delta_t
+        self.position += self.colored_noise * self.delta_t
+        self.position += self.rng.normal(
             loc=0.0,  # * mean
             scale=np.sqrt(2 * self.temperature / \
                           self.gamma * self.delta_t),  # * std
             size=(self.N_ensemble, self.N_particle),
         )
-
-        self.position[0] = 1 / 2 * (self.position[0] + temp_position)
-        self.position[1] = temp_position
-
+        assert self.position.shape == (
+            self.N_ensemble, self.N_particle), f"position shape mismatch: {self.position.shape}"
         self.Time.position_update += time.perf_counter() - now
 
-        now = time.perf_counter()
         self.update_periodic_boundary()
-        self.Time.periodic_update += time.perf_counter() - now
-
-        now = time.perf_counter()
         self.update_positive_negative()
-        self.Time.positive_update += time.perf_counter() - now
 
         now = time.perf_counter()
-        temp_noise = self.colored_noise[1] - \
-            self.colored_noise[1] / self.tau * self.delta_t
-        temp_noise += self.rng.normal(
+        self.colored_noise += -self.colored_noise / self.tau * self.delta_t
+        self.colored_noise += self.rng.normal(
             loc=0.0,  # * mean
             scale=np.sqrt(2 * self.Da / self.tau**2 * self.delta_t),  # * std
             size=(self.N_ensemble, self.N_particle)
         )
-
-        self.colored_noise[0] = 1 / 2 * (self.colored_noise[0] + temp_noise)
-        self.colored_noise[1] = temp_noise
-
+        assert self.colored_noise.shape == (
+            self.N_ensemble, self.N_particle), f"position shape mismatch: {self.colored_noise.shape}"
         self.Time.colored_noise_update += time.perf_counter() - now
 
     def get_force(self) -> npt.NDArray:
+        now = time.perf_counter()
 
-        force = np.zeros(shape=(2, self.N_ensemble, self.N_particle))
+        force = np.zeros(shape=(self.N_ensemble, self.N_particle))
 
         force[self.positive] = self.slope
         force[self.negative] = - self.slope
 
+        self.Time.force_update += time.perf_counter() - now
+
         return force
 
     def get_drag(self) -> npt.NDArray:  # * calculate drag force
-
         now = time.perf_counter()
 
-        positive_drag = self.positive[1].astype(np.int64).sum(axis=1)
-        negative_drag = self.negative[1].astype(np.int64).sum(axis=1)
+        positive_drag = self.positive.astype(np.int64).sum(axis=1)
+        negative_drag = self.negative.astype(np.int64).sum(axis=1)
 
-        assert len(positive_drag) == self.N_ensemble
-        assert len(negative_drag) == self.N_ensemble
+        assert (positive_drag - negative_drag).shape == (self.N_ensemble, )
 
         self.Time.drag_update += time.perf_counter() - now
 
         return positive_drag - negative_drag
 
     def update_positive_negative(self) -> None:
+        now = time.perf_counter()
+
         self.positive = (
             0.0 < self.position) & (self.position < self.Lambda / 2)
         self.negative = (
             - self.Lambda / 2 < self.position) & (self.position < 0.0)
 
+        self.Time.positive_update += time.perf_counter() - now
+
     # * periodic boundary condition
     def update_periodic_boundary(self) -> None:
+        now = time.perf_counter()
 
         self.position[self.position < -self.boundary / 2] += self.boundary
         self.position[self.position > self.boundary / 2] -= self.boundary
 
-    def animation(self, frames: int = 1000) -> None:  # * animate histogram
+        self.Time.periodic_update += time.perf_counter() - now
+
+    def histogram(self, frames: int = 1000) -> None:  # * animate histogram
         self.fig, self.ax = plt.subplots(tight_layout=True)
         self.bins = np.linspace(-self.boundary/2,
                                 self.boundary/2, self.N_bins+1)
 
-        self.ax.hist(self.position[1, 0], bins=self.bins)
+        self.ax.hist(self.position[0], bins=self.bins)
         self.ax.set_xlim(left=-self.boundary/2, right=self.boundary/2)
         self.ax.set_ylim(bottom=0.0, top=self.N_particle/self.N_bins*1.5)
+
+        def animate_histogram(i: int) -> None:  # * update animation
+            print(i, end=" ")
+            for _ in range(self.interval):
+                self.time_evolution()
+
+            self.ax.cla()
+            self.ax.hist(self.position[0], bins=self.bins)
+            self.ax.axvline(-self.Lambda/2, linestyle="--", color="k")
+            self.ax.axvline(0.0, linestyle="--", color="k")
+            self.ax.axvline(self.Lambda/2, linestyle="--", color="k")
+            self.ax.axhline(self.N_particle / self.N_bins,
+                            linestyle="--", color="k")
+
+            lx = np.linspace(-self.Lambda/2, 0, 10)
+            rx = np.linspace(0, self.Lambda/2, 10)
+
+            def f(x: npt.NDArray):
+                return self.N_particle/self.N_bins * (1 + x / (self.Lambda / 2))
+
+            def g(x: npt.NDArray):
+                return self.N_particle/self.N_bins * (1 - x / (self.Lambda / 2))
+
+            self.ax.plot(lx, f(lx), color="b")
+            self.ax.plot(rx, g(rx), color="r")
+
+            self.ax.set_xlim(left=-self.boundary/2, right=self.boundary/2)
+            self.ax.set_ylim(bottom=0.0, top=self.N_particle/self.N_bins*1.5)
+
+            self.ax.text(
+                0.99, 0.99, f"iter = {self.interval * (i+1)}",
+                verticalalignment="top", horizontalalignment='right',
+                transform=self.ax.transAxes,
+                color='black', fontsize=20
+            )
+
+            self.ax.text(
+                0.99, 0.91, f"drag = {self.get_drag()[0]}",
+                verticalalignment="top", horizontalalignment='right',
+                transform=self.ax.transAxes,
+                color='black', fontsize=20
+            )
 
         ani = animation.FuncAnimation(
-            fig=self.fig, func=self.update, frames=frames, interval=0, blit=False)
+            fig=self.fig, func=animate_histogram, frames=frames, interval=0, blit=False)
 
-        ani.save(f"frames={frames}.mp4", fps=30,
+        ani.save(f"animation/histogram ptcl={self.N_particle} iter={frames * self.interval}.mp4", fps=30,
                  extra_args=['-vcodec', 'libx264'])
 
-    def update(self, i: int) -> None:  # * update animation
-        print(i, end=" ")
-        self.time_evolution()
-
-        self.ax.cla()
-        self.ax.hist(self.position[1, 0], bins=self.bins)
-        self.ax.axvline(-self.Lambda/2, linestyle="--", color="k")
-        self.ax.axvline(0.0, linestyle="--", color="k")
-        self.ax.axvline(self.Lambda/2, linestyle="--", color="k")
-        self.ax.axhline(self.N_particle / self.N_bins,
-                        linestyle="--", color="k")
-
-        lx = np.linspace(-self.Lambda/2, 0, 10)
-        rx = np.linspace(0, self.Lambda/2, 10)
-
-        def f(x: npt.NDArray):
-            return self.N_particle/self.N_bins * (1 + x / (self.Lambda / 2))
-
-        def g(x: npt.NDArray):
-            return self.N_particle/self.N_bins * (1 - x / (self.Lambda / 2))
-
-        self.ax.plot(lx, f(lx), color="b")
-        self.ax.plot(rx, g(rx), color="r")
-
-        self.ax.set_xlim(left=-self.boundary/2, right=self.boundary/2)
-        self.ax.set_ylim(bottom=0.0, top=self.N_particle/self.N_bins*1.5)
-
-        self.ax.text(
-            0.99, 0.99, f"iter = {i+1}",
-            verticalalignment="top", horizontalalignment='right',
-            transform=self.ax.transAxes,
-            color='black', fontsize=20
-        )
-
-        self.ax.text(
-            0.99, 0.91, f"drag = {self.get_drag()[0]}",
-            verticalalignment="top", horizontalalignment='right',
-            transform=self.ax.transAxes,
-            color='black', fontsize=20
-        )
-
     def phase_space(self, frames: int = 1000, fps: int = 100) -> None:
-        init_position = np.full(
-            shape=(self.N_ensemble, self.N_particle), fill_value=0.0)
-        self.position = np.array([init_position, init_position])
-
         self.fig, self.ax = plt.subplots(tight_layout=True)
         self.ax.set_xlim([-self.boundary/2, self.boundary/2])
         self.ax.set_ylim([-3, 3])
 
         position_list, noise_list = [], []
-        self.line, = self.ax.plot(position_list, noise_list)
+        self.ax.scatter(position_list, noise_list)
 
         def animate_phase_space(i: int):
             print(i, end=" ")
-            self.time_evolution()
+            for _ in range(self.interval):
+                self.time_evolution()
 
             self.ax.cla()
             self.ax.set_xlim([-self.boundary/2, self.boundary/2])
             self.ax.set_ylim([-3, 3])
-            position_list.append(self.position[1, 0, 0])
-            noise_list.append(self.colored_noise[1, 0, 0])
-            self.line, = self.ax.plot(position_list, noise_list)
+            position_list.extend(self.position[0])
+            noise_list.extend(self.colored_noise[0])
+            self.ax.scatter(position_list, noise_list, s=1)
+            self.ax.axvline(self.Lambda / 2, linestyle="--", color="red")
+            self.ax.axvline(-self.Lambda / 2, linestyle="--", color="red")
 
-            self.ax.set_title(f"Phase space", fontsize=25)
+            self.ax.set_title(
+                f"Phase space ptcl={self.N_particle} interval={self.interval}", fontsize=20)
             self.ax.set_xlabel("Particle position", fontsize=20)
             self.ax.set_ylabel("Colored noise", fontsize=20)
 
             self.ax.text(
-                0.99, 0.99, f"iteration = {i+1}\ndelta t = {self.delta_t}",
+                0.99, 0.99, f"iteration = {(i+1)*self.interval}",
                 verticalalignment="top", horizontalalignment='right',
                 transform=self.ax.transAxes,
                 color='black', fontsize=20
@@ -327,20 +326,8 @@ class AOUP:
         ani = animation.FuncAnimation(
             fig=self.fig, func=animate_phase_space, frames=frames, blit=False)
 
-        ani.save(f"phase space frames={frames}.mp4", fps=fps,
+        ani.save(f"animation/phase space ptcl={self.N_particle} iter={frames * self.interval}.mp4", fps=fps,
                  extra_args=['-vcodec', 'libx264'])
-
-
-# * get external force from object
-@njit
-def get_force(N_ensemble, N_particle, positive, negative, slope) -> npt.NDArray:
-
-    force = np.zeros(shape=(N_ensemble, N_particle)).reshape(-1)
-
-    force[positive.reshape(-1)] = slope
-    force[negative.reshape(-1)] = - slope
-
-    return force.reshape(N_ensemble, N_particle)
 
 
 def get_logspace(
@@ -406,7 +393,7 @@ if __name__ == '__main__':
         )
 
         aoup = AOUP(parameter)
-        aoup.get_result()
+        aoup.run_AOUP()
 
     elif args.mode == "velocity":
         velocities = get_logspace(
@@ -434,7 +421,7 @@ if __name__ == '__main__':
             # print(parameter)
 
             aoup = AOUP(parameter)
-            aoup.get_result()
+            aoup.run_AOUP()
 
     elif args.mode == "Lambda":
         Lambdas = get_logspace(
@@ -462,7 +449,7 @@ if __name__ == '__main__':
             # print(parameter)
 
             aoup = AOUP(parameter)
-            aoup.get_result()
+            aoup.run_AOUP()
 
     elif args.mode == "slope":
         slopes = get_logspace(
@@ -490,7 +477,7 @@ if __name__ == '__main__':
             # print(parameter)
 
             aoup = AOUP(parameter)
-            aoup.get_result()
+            aoup.run_AOUP()
 
     else:
         raise ValueError(
